f7615830e8693e4f314b10c61eb859753a133fb4
Joao <joaogabriel.farias@hotmail.com>
2026-06-07 19:34:21 -0300

    Funções da classe discentes implementadas, menu do sistema criado

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
-from dataclasses import dataclass
+from dataclasses import dataclass, field
 
 @dataclass
 class discente:
     matricula: str
     nome: str
     num_presencas: int = 0
-    notas: list[int] = []
+    notas: list[float] = field(default_factory=lambda: [])
 
 class discentes:
     def __init__(self):
@@ -13,19 +13,102 @@ class discentes:
 
 
     @property
-    def discentes(self):
+    def discentes_dict(self):
         return self._discentes
 
 
-    def adicionar_discente(self, matricula: str, nome: str):
-        if  matricula in self._discentes.keys():
-            print("Matrícula já cadastrada")
+    def listar_discentes(self):
+        if not self._discentes:
+            print("Não existem discentes cadastrados")
             return
-        self._discentes.append(discente(matricula=matricula, nome=nome))
-        print(f"Discente {nome}, de matrícula {matricula} cadastrado com sucesso")
-    
+        for matricula, d in self._discentes.items():
+            print(f"Matrícula: {d.matricula} | Nome: {d.nome} | Presenças: {d.num_presencas} | Notas: {d.notas}")
+
+
+    def adicionar_discente(self, matricula: str, nome: str):
+        if matricula in self._discentes:
+            print("Erro: Matrícula já cadastrada")
+            return
+        self._discentes[matricula] = discente(matricula=matricula, nome=nome)
+        print(f"Discente {matricula} | {nome} cadastrado com sucesso")
+
+
+    def atualizar_notas(self, matricula: str, notas: list[float]):
+        print("nd")
+
+
+    def atualizar_frequencia(self, matricula: str):
+        print("nd")
+
 
     def remover_discente(self, matricula: str):
-        if matricula in self._discentes.keys():
+        if matricula in self._discentes:
             self._discentes.pop(matricula)
 
+
+    def imprimir_relatorio(self):
+        aprovados_nota = 0
+        reprovados_falta = 0
+        for d in self._discentes.values():
+            media = sum(d.notas) / 3
+
+            frequencia = d.num_presencas / 30
+            
+            if media >= 7.0:
+                aprovados_nota += 1
+            if frequencia < 0.75:
+                reprovados_falta += 1
+                
+        print(f"Quantidade de discentes aprovados por nota: {aprovados_nota}")
+        print(f"Quantidade de discentes reprovados por falta: {reprovados_falta}")
+
+def main():
+    menu = discentes()
+    
+    while True:
+        print("--- Menu ---")
+        print("1. Listar discentes")
+        print("2. Cadastrar discente")
+        print("3. Atualizar notas")
+        print("4. Atualizar frequência")
+        print("5. Remover discente")
+        print("6. Imprimir relatório")
+        print("7. Sair")
+        
+        opcao = input("Escolha uma opção: ")
+        
+        match opcao:
+            case '1':
+                menu.listar_discentes()
+                
+            case '2':
+                matricula = input("Digite o número de matrícula: ")
+                nome = input("Digite o nome: ")
+                menu.adicionar_discente(matricula, nome)
+                
+            case '3':
+                matricula = input("Digite o número de matrícula: ")
+                nota1 = float(input("Digite a nota 1: "))
+                nota2 = float(input("Digite a nota 2: "))
+                nota3 = float(input("Digite a nota 3: "))
+                menu.atualizar_notas(matricula, [nota1, nota2, nota3])
+                    
+            case '4':
+                matricula = input("Digite a matrícula do discente: ")
+                menu.atualizar_frequencia(matricula)
+                
+            case '5':
+                matricula = input("Digite a matrícula do discente a ser removido: ")
+                menu.remover_discente(matricula)
+                
+            case '6':
+                menu.imprimir_relatorio()
+                
+            case '7':
+                print("Saindo do programa.")
+                break
+                
+            case _:
+                print("Opção inválida.")
+
+main()
